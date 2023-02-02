15b411e3772a11ba91154ff9ce7f65101c4e0027
BeomsikSeo <124111121+BeomsikSeo@users.noreply.github.com>
2023-02-02 17:32:49 +0900

    4일차 프로젝트

---
+++ day04/out2.js
@@ -0,0 +1,13 @@
+function 여행하다() {
+    console.log("1. 돈을 마련한다");
+    console.log("2. 어디로 갈지 알아본다");
+    console.log("3. 교통편을 알아본다");
+    console.log("4. 짐을 싼다");
+    console.log("5. 출발~");
+}
+
+function 쉬다() {
+    console.log("1. 이불을 편다");
+    console.log("2. 눕는다");
+    console.log("3. 휴대폰을 본다");
+}
